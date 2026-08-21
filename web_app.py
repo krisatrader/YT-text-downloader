3203005e19102b25107ff220549339a252d3d2d3
krisatrader <sebestyen.kristof@gmail.com>
2026-08-21 10:13:41 +0200

    Enable auto-reload on web server

--- web_app.py
+++ web_app.py
@@ -710,6 +710,4 @@ if __name__ == "__main__":
     import uvicorn
     print("=" * 60)
     print("🚀 YouTube Átirat Letöltő Web Szerver Indulása...")
-    print("🌐 Elérési út a böngészőben: http://127.0.0.1:8000")
-    print("=" * 60)
-    uvicorn.run("web_app:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("web_app:app", host="127.0.0.1", port=8000, reload=True)
